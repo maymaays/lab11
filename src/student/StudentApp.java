71bc54620decb979176de649331456688877086c
Methawee A <merimo.mayjiiex@hotmail.com>
2016-05-05 17:48:52 +0700

    without javadoc

--- src/student/StudentApp.java
+++ src/student/StudentApp.java
@@ -1,28 +1,46 @@
 package student;
+
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.time.LocalDate;
 
 /**
  * Display reminders of students having a birthday soon.
- * @author you
+ * @author Methawee Apinainarong 5810546714
  */
 public class StudentApp {
 
 	/**
-	 * Print the names (and birthdays) of students having a birtday in the
-	 * specified month.
+	 * Print the names (and birthdays) of students having a birtday in the specified month.
 	 * @param students list of students
-	 * @param month the month to use in selecting bithdays
+	 * @param month
+	 *            the month to use in selecting bithdays
 	 */
-	public void filterAndPrint( List<Student> students, int month ) {
-		for(Student s : students ) {
-			if (s.getBirthdate().getMonthValue() == month)
-	                  System.out.println( s );
-		}
+	public void filterAndPrint(List<Student> students, Predicate<Student> filter, Consumer<Student> action,
+			Comparator<Student> compare) {
+
+		students.stream().filter(filter).sorted(compare).forEach(action);
 	}
-	
+
 	public static void main(String[] args) {
 		List<Student> students = Registrar.getInstance().getStudents();
+		
+		Predicate<Student> birthdaytest = (s) -> (s.getBirthdate().getDayOfYear() > LocalDate.now().getDayOfYear())
+				&& (s.getBirthdate().getDayOfYear() < (LocalDate.now().getDayOfYear() + 14));
+		
+		
+		Consumer<Student> consumer = (s) -> System.out.println(s.getFirstname() + " will have birthday on "
+				+ s.getBirthdate().getDayOfMonth() + " " + s.getBirthdate().getMonth() + ".");
+		Comparator<Student> byName = (a, b) -> a.getFirstname().charAt(0) - b.getFirstname().charAt(0);
+		Comparator<Student> byBirthday = (a, b) -> a.getBirthdate().getMonth().getValue() -
+				b.getBirthdate().getMonth().getValue();
+		
 		StudentApp app = new StudentApp();
-		app.filterAndPrint(students, 5 /* may */);
+		app.filterAndPrint(students, birthdaytest, consumer, byBirthday);
+	
+
 	}
+
 }
